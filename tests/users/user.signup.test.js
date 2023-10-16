598b7614b37589eda8a785663243eee38b303187
dana <aidana1529@gmail.com>
2023-10-16 16:47:48 +0900

    feat: unittest 에러 추가

--- tests/users/user.signup.test.js
+++ tests/users/user.signup.test.js
@@ -20,11 +20,32 @@ beforeAll(async () => {
     .expect({ message: "USER_CREATED" })
   });
 
+  test("FAIL: key error", async () => {
+    await request(app)
+    .post("/users/")
+    .send({password: "jdjA!12", nickname: "dana", phoneNumber: "1234"})
+    .expect({ error: "400 KEY_ERROR" })
+  })
+
   test("FAIL: already registered", async () => {
     await request(app)
     .post("/users/")
     .send({email: "agjjj@mail.com", password: "jdjA!12", nickname: "dana", phoneNumber: "1234"})
     .expect({ error: "400 ALREADY_REGISTERED" })
+  })
+
+  test ("FAIL: invalid email", async () => {
+    await request(app)
+    .post("/users/")
+    .send({email: "agjjjmail.com", password: "jdjA!12", nickname: "dana", phoneNumber: "1234"})
+    .expect({ error: "400 EMAIL_INVALID" })
+  })
+
+  test ("FAIL: invalid password", async () => {
+    await request(app)
+    .post("/users/")
+    .send({email: "agjjj@mail.com", password: "jdjA!", nickname: "dana", phoneNumber: "1234"})
+    .expect({ error: "400 PASSWORD_INVALID" })
   })
 });
  
