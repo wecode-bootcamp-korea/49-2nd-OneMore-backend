df4044120c7b99d09f41f04234128e958de48de4
JISUlicious <ljshong@hanmail.net>
2023-10-18 21:07:37 +0900

    feat: add test case for utils

---
+++ tests/utils/throwError.test.js
@@ -0,0 +1,33 @@
+const {log} = require("console");
+
+const { throwError } = require("../../src/utils");
+
+
+describe("TEST throeError", () => {
+  test("SUCCESS: 400 KEY_ERROR", () => {
+    try {
+      throwError(400, "KEY_ERROR");
+    } catch (error) {
+      expect(error.message).toBe("KEY_ERROR");
+      expect(error.status).toBe(400);
+    }
+  });
+
+  test("SUCCESS: string status code", () => {
+    try {
+      throwError("string", "KEY_ERROR");
+    } catch (error) {
+      expect(error.message).toBe("KEY_ERROR");
+      expect(error.status).toBe(500);
+    }
+  });
+
+  test("SUCCESS: string type integer status code", () => {
+    try {
+      throwError("400", "KEY_ERROR");
+    } catch (error) {
+      expect(error.message).toBe("KEY_ERROR");
+      expect(error.status).toBe(400);
+    }
+  });
+});
