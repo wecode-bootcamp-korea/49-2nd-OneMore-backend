048bdb2f3020a228584e7ee20691ad0432159854
ayoung <say199532@gmail.com>
2023-10-21 13:55:18 +0900

    fix: 리뷰반영

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -17,7 +17,6 @@ const kakaoStrategy = new KakaoStrategy(
     callbackURL: process.env.KAKAO_CALLBACK_URL,
   },
   async (accessToken, refreshToken, profile, done) => {
-    console.log("kakaoprofile: ", profile)
     const email = profile._json.kakao_account.email;
     const nickname = profile._json.kakao_account.profile.nickname;
     const socialUid = profile.id;
@@ -25,7 +24,6 @@ const kakaoStrategy = new KakaoStrategy(
     try {
       // 소셜 로그인 계정 유/무 확인
       const exisitingUserBySocial = await userDao.findUserBySocial(socialUid, socialProvider);
-      console.log("exisitingUserBySocial: ", exisitingUserBySocial)
       if (exisitingUserBySocial) {
         const { accessToken, refreshToken } = await generateTokens(exisitingUserBySocial.id);
         console.log("generateTokens: ", accessToken, refreshToken)
@@ -33,14 +31,12 @@ const kakaoStrategy = new KakaoStrategy(
       }
       // 기존 email 유/무 확인
       const [exisitingUserByEmail] = await userDao.existingUser(email);
-      console.log("exisitingUserByEmail: ", exisitingUserByEmail)
       if (exisitingUserByEmail) {
-        await userDao.updateUserBySocial(socialUid, socialProvider, email)
+        await userDao.updateUserBySocial(userId, socialUid, socialProvider)
         const { accessToken, refreshToken } = await generateTokens(exisitingUserByEmail.id);
         return done(null, { accessToken, refreshToken, nickname }); // => 소셜계정 업데이트 후, 토큰 발행
       } else {
         const createdUserBySocial = await userDao.createUserBySocial(email, nickname, socialUid, socialProvider)
-        console.log("createdUserBySocial: ", createdUserBySocial)
         const { accessToken, refreshToken } = await generateTokens(createdUserBySocial.id);
         return done(null, { accessToken, refreshToken, nickname }); // => 소셜유저 생성 후, 토큰 발행  
       }
