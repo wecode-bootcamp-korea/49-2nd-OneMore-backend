c3aedfd5c908e2ffd4fc0c5f1e52c1d6a307adfc
dana <aidana1529@gmail.com>
2023-10-24 19:58:02 +0900

    test: env.sample에 구글소셜로그인 내용 추가

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { AppDataSource } = require("./src/models/dataSource");
 const start = async () => {
   await AppDataSource.initialize();
 
-  app.listen(8000, () => {
+  app.listen(3000, () => {
     console.log(`server is running`);
   });
 };
