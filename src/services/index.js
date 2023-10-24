d580b8e5bdffaf86d155d14f028e066e02947663
JISUlicious <ljshong@hanmail.net>
2023-10-24 16:47:52 +0900

    test: test for GET /exercises/recommended

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
 const exerciseService = require("./exerciseService");
 const routineService = require("./routineService");
+const userService = require("./userService");
 
 module.exports = {
   exerciseService,
---
+++ tests/exercises/exercises.getRecommendedExercises.test.js
@@ -0,0 +1,92 @@
+const request = require("supertest");
+const { log } = require("console");
+
+const { app } = require("../../app");
+const { AppDataSource } = require("../../src/models/dataSource");
+
+jest.mock("../../src/middleware/tokenValidation");
+
+const tokenMiddleware = require("../../src/middleware/tokenValidation");
+const middlewareSpy = jest.spyOn(tokenMiddleware, "tokenValidation");
+
+describe("TEST: /exercises/recommended GET method", () => {
+  beforeAll(async () => {
+    await AppDataSource.initialize();
+    await AppDataSource.query(`
+      INSERT INTO users
+        (id, nickname, email, subscription_state, password)
+      VALUES
+        (1, 'Park-KJ', 'rudwos6@naver.com', 1, 'password001.'),
+        (2, 'Hong-JS', 'jisu@naver.com', 0, 'password001.')
+      ;
+    `);
+    await AppDataSource.query(`
+      INSERT INTO exercise_categories
+        (name)
+      VALUES
+        ('testCategory1'),
+        ('testCategory2'),
+        ('testCategory3'),
+        ('testCategory4')
+      ;
+    `);
+    await AppDataSource.query(`
+      INSERT INTO exercises
+        (name, video_url, thumbnail_url, is_premium, exercise_category, duration_in_seconds_per_set, set_counts)
+      VALUES
+        ('testExercise1', 'testVideoUrl', 'testThumbnailUrl', 0, 1, 60, 1),
+        ('testExercise2', 'testVideoUrl', 'testThumbnailUrl', 0, 1, 60, 2),
+        ('testExercise3', 'testVideoUrl', 'testThumbnailUrl', 0, 1, 60, 3),
+        ('testExercise4', 'testVideoUrl', 'testThumbnailUrl', 0, 1, 60, 4),
+        ('testExercise5', 'testVideoUrl', 'testThumbnailUrl', 0, 1, 60, 5),
+        ('testExercise6', 'testVideoUrl', 'testThumbnailUrl', 1, 1, 60, 6),
+        ('testExercise7', 'testVideoUrl', 'testThumbnailUrl', 1, 1, 60, 7),
+        ('testExercise8', 'testVideoUrl', 'testThumbnailUrl', 1, 1, 60, 8),
+        ('testExercise9', 'testVideoUrl', 'testThumbnailUrl', 1, 1, 60, 9)
+      ;
+    `);
+    log("database initialized for test");
+  });
+
+  afterEach(() => {
+    // restore the spy created with spyOn
+    jest.restoreAllMocks();
+  });
+
+  afterAll(async () => {
+    await AppDataSource.query(`SET foreign_key_checks = 0`);
+    await AppDataSource.query(`TRUNCATE users`);
+    await AppDataSource.query(`TRUNCATE exercises`);
+    await AppDataSource.query(`TRUNCATE exercise_categories`);
+    await AppDataSource.query(`SET foreign_key_checks = 1`);
+    await AppDataSource.destroy();
+  });
+
+  test("SUCCESS: success without subscription", async () => {
+    middlewareSpy.mockImplementation((req, res, next) => {
+      req.userId = 2;
+      next();
+    });
+
+    const result = await request(app).get("/exercises/recommended");
+    
+    expect(result.status).toBe(200);
+    result.body.data.exercises.forEach((item) => {
+      expect(item.isPremium).toBe(0);
+    });
+  });
+
+  test("SUCCESS: success with subscription", async () => {
+    middlewareSpy.mockImplementation((req, res, next) => {
+      req.userId = 1;
+      next();
+    });
+
+    const result = await request(app).get("/exercises/recommended");
+
+    expect(result.status).toBe(200);
+    result.body.data.exercises.forEach((item) => {
+      expect([0, 1]).toContain(item.isPremium);
+    });
+  });
+});
